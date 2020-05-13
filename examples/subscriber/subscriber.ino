03caa461b0be4c21ffa1f061d20ce00b93c0fe42
Kei <kkw@robotis.com>
2020-05-13 14:49:44 +0900

    Add LED_BUILTIN definition in subscribe example for some esp32 boards.

--- examples/subscriber/subscriber.ino
+++ examples/subscriber/subscriber.ino
@@ -1,6 +1,10 @@
 #include "user_config.h" // It must be located above ros2arduino.h.
 #include <ros2arduino.h>
 
+#ifndef LED_BUILTIN // To support some boards (eg. some esp32 boards)
+#define LED_BUILTIN 13
+#endif 
+
 #define XRCEDDS_PORT  Serial 
 
 void subscribeLed(std_msgs::Bool* msg, void* arg)
